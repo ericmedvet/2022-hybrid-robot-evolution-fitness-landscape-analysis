d5e2397e6e4ce5861816fff845f4ab775e5c6b74
Eric Medvet <eric.medvet@gmail.com>
2022-10-12 18:39:54 +0200

    Working on phase 2.

--- it.units.erallab.respap.hrefla.main/src/main/java/it/units/erallab/respap/hrefla/Starter.java
+++ it.units.erallab.respap.hrefla.main/src/main/java/it/units/erallab/respap/hrefla/Starter.java
@@ -16,8 +16,73 @@
 
 package it.units.erallab.respap.hrefla;
 
-public class Starter {
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class Starter implements Runnable {
+
+  private final static Logger L = Logger.getLogger(Starter.class.getName());
+
+  @Parameter(
+      names = {"--evoResultFile", "-f"},
+      description = "Path of the file with the results of the evolution"
+  )
+  public String evoResultsFile = "";
+  @Parameter(
+      names = {"--iteration", "-i"},
+      description = "Iteration index to extract best individuals"
+  )
+  public List<Integer> iterations = List.of();
+  @Parameter(
+      names = {"--nOfTargets", "-nt"},
+      description = "Number of target genotypes for each best"
+  )
+  public int nOfTargets = 1;
+  @Parameter(
+      names = {"--nOfPoints", "-np"},
+      description = "Number of points for each best-to-target section"
+  )
+  public int nOfPoints = 10;
+
+  private record TargetGenotype(
+      String target
+  ) {}
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    try {
+      Starter starter = new Starter();
+      JCommander.newBuilder()
+          .addObject(starter)
+          .build()
+          .parse(args);
+      starter.run();
+    } catch (ParameterException e) {
+      e.usage();
+      L.severe(String.format("Cannot read command line options: %s", e));
+      System.exit(-1);
+    } catch (RuntimeException e) {
+      L.severe(e.getClass().getSimpleName() + ": " + e.getMessage());
+      System.exit(-1);
+    }
   }
+
+  @Override
+  public void run() {
+    //check data
+    try (BufferedReader br = new BufferedReader(new FileReader(evoResultsFile))) {
+
+    } catch (IOException e) {
+      throw new IllegalArgumentException("Cannot open file %s: %s".formatted(evoResultsFile, e));
+    }
+    //read file and save bests
+    //iterate over bests
+  }
+
 }
---
+++ it.units.erallab.respap.hrefla.main/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module it.units.erallab.respap.hrefla.main {
+  requires jcommander;
+  requires java.logging;
+  exports it.units.erallab.respap.hrefla to jcommander;
+}
