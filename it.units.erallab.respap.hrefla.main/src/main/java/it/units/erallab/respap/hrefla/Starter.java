c43924f4ca33be91cd3d949cd201d6d67f3c7f5e
Eric Medvet <eric.medvet@gmail.com>
2022-10-13 23:40:25 +0200

    Basic structure done.

--- it.units.erallab.respap.hrefla.main/src/main/java/it/units/erallab/respap/hrefla/Starter.java
+++ it.units.erallab.respap.hrefla.main/src/main/java/it/units/erallab/respap/hrefla/Starter.java
@@ -19,11 +19,27 @@ package it.units.erallab.respap.hrefla;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import it.units.erallab.mrsim2d.builder.NamedBuilder;
+import it.units.erallab.mrsim2d.core.Agent;
+import it.units.erallab.mrsim2d.core.EmbodiedAgent;
+import it.units.erallab.mrsim2d.core.engine.Engine;
+import it.units.erallab.mrsim2d.core.tasks.Task;
+import it.units.erallab.robotevo2d.main.builder.MapperBuilder;
+import it.units.erallab.robotevo2d.main.singleagent.PreparedNamedBuilder;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+import java.util.ServiceLoader;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.logging.Logger;
 
 public class Starter implements Runnable {
@@ -31,12 +47,17 @@ public class Starter implements Runnable {
   private final static Logger L = Logger.getLogger(Starter.class.getName());
 
   @Parameter(
-      names = {"--evoResultFile", "-f"},
-      description = "Path of the file with the results of the evolution"
+      names = {"--inputFile", "-if"},
+      description = "Path of the input file with the results of the evolution"
   )
-  public String evoResultsFile = "";
+  public String inputFile = "";
   @Parameter(
-      names = {"--iteration", "-i"},
+      names = {"--outputFile", "-of"},
+      description = "Path of the file for the output"
+  )
+  public String outputFile = "";
+  @Parameter(
+      names = {"--iterations", "-i"},
       description = "Iteration index to extract best individuals"
   )
   public List<Integer> iterations = List.of();
@@ -46,14 +67,48 @@ public class Starter implements Runnable {
   )
   public int nOfTargets = 1;
   @Parameter(
+      names = {"--targetDistance", "-d"},
+      description = "Euclidean distance of the target"
+  )
+  public double targetDistance = 0.5;
+  @Parameter(
       names = {"--nOfPoints", "-np"},
       description = "Number of points for each best-to-target section"
   )
   public int nOfPoints = 10;
+  @Parameter(
+      names = {"--task"},
+      description = "Task description"
+  )
+  public String task = "";
+  @Parameter(
+      names = {"--qExtractor"},
+      description = "Quality extraction function"
+  )
+  public String qExtractor = "";
+  @Parameter(
+      names = {"--nOfThreads"},
+      description = "Number of threads"
+  )
+  public int nOfThreads = 1;
+
+  private record Outcome(
+      TargetGenotype targetGenotype,
+      int targetIndex,
+      int pointIndex,
+      double d,
+      List<Double> genotype,
+      double q
+  ) {}
 
   private record TargetGenotype(
-      String target
+      String target,
+      String mapper,
+      String randomGenerator,
+      int iteration,
+      List<Double> genotype
   ) {}
+
 
   public static void main(String[] args) {
     try {
@@ -73,16 +128,81 @@ public class Starter implements Runnable {
     }
   }
 
+  public static Function<String, Object> javaDeserializer() {
+    return s -> {
+      try (ByteArrayInputStream bais = new ByteArrayInputStream(Base64.getDecoder().decode(s)); ObjectInputStream ois = new ObjectInputStream(
+          bais)) {
+        return ois.readObject();
+      } catch (IOException | ClassNotFoundException e) {
+        L.severe("Cannot deserialize: %s".formatted(e));
+      }
+      return null;
+    };
+  }
+
   @Override
   public void run() {
-    //check data
-    try (BufferedReader br = new BufferedReader(new FileReader(evoResultsFile))) {
-
-    } catch (IOException e) {
-      throw new IllegalArgumentException("Cannot open file %s: %s".formatted(evoResultsFile, e));
-    }
+    NamedBuilder<?> nb = PreparedNamedBuilder.get();
     //read file and save bests
+    List<TargetGenotype> targetGenotypes;
+    try (Reader reader = new FileReader(inputFile)) {
+      L.info("Reading input file %s".formatted(inputFile));
+      CSVParser parser = CSVFormat.Builder.create().setDelimiter(';').setHeader().build().parse(reader);
+      //noinspection unchecked
+      targetGenotypes = parser.stream()
+          .filter(r -> iterations.contains(Integer.parseInt(r.get("iterations"))))
+          .map(r -> new TargetGenotype(
+              r.get("target"),
+              r.get("mapper"),
+              r.get("randomGenerator"),
+              Integer.parseInt(r.get("iterations")),
+              (List<Double>)javaDeserializer().apply(r.get("best→g"))
+          ))
+          .toList();
+      L.info("%d target genotypes found for iterations %s".formatted(targetGenotypes.size(), iterations));
+    } catch (IOException e) {
+      throw new IllegalArgumentException("Cannot open file %s: %s".formatted(inputFile, e));
+    }
+    //prepare engine and executor and qExtractor
+    Supplier<Engine> engineSupplier = () -> ServiceLoader.load(Engine.class)
+        .findFirst()
+        .orElseThrow(() -> new RuntimeException("Cannot instantiate an engine"));
+    ExecutorService executorService = Executors.newFixedThreadPool(nOfThreads);
+    @SuppressWarnings("unchecked") Function<Object, Double> qExtractorF =
+        (Function<Object, Double>) nb.build(qExtractor);
     //iterate over bests
+    List<Future<Outcome>> futures = new ArrayList<>();
+    for (TargetGenotype targetGenotype : targetGenotypes) {
+      for (int targetIndex = 0; targetIndex < nOfTargets; targetIndex = targetIndex + 1) {
+        int t = targetIndex;
+        //TODO build random target point
+        for (int pointIndex = 0; pointIndex < nOfPoints; pointIndex = pointIndex + 1) {
+          int p = pointIndex;
+          double d = 0; //TODO
+          List<Double> genotype = targetGenotype.genotype(); //TODO
+          Supplier<EmbodiedAgent> unmappedAgentSupplier = () -> (EmbodiedAgent) nb.build(targetGenotype.target());
+          @SuppressWarnings("unchecked") MapperBuilder<List<Double>, Supplier<EmbodiedAgent>> mapper =
+              (MapperBuilder<List<Double>, Supplier<EmbodiedAgent>>) nb.build(
+                  targetGenotype.mapper());
+          Supplier<EmbodiedAgent> agentSupplier = mapper.buildFor(unmappedAgentSupplier).apply(genotype);
+          @SuppressWarnings("unchecked") Task<Supplier<Agent>, ?> localTask =
+              (Task<Supplier<Agent>, ?>) nb.build(task);
+          futures.add(executorService.submit(() -> {
+            Object taskOutcome = localTask.run(agentSupplier::get, engineSupplier.get());
+            L.info("Task done with outcome %s".formatted(taskOutcome));
+            return new Outcome(targetGenotype, t, p, 0, genotype, qExtractorF.apply(taskOutcome));
+          }));
+        }
+      }
+    }
+    //save results
+    futures.forEach(f -> {
+      try {
+        L.info("%s".formatted(f.get())); // TODO save
+      } catch (InterruptedException | ExecutionException e) {
+        L.severe("Cannot get results due to: %s".formatted(e));
+      }
+    });
   }
 
 }
--- it.units.erallab.respap.hrefla.main/src/main/java/module-info.java
+++ it.units.erallab.respap.hrefla.main/src/main/java/module-info.java
@@ -1,5 +1,11 @@
 module it.units.erallab.respap.hrefla.main {
   requires jcommander;
   requires java.logging;
+  requires commons.csv;
+  uses it.units.erallab.mrsim2d.core.engine.Engine;
+  requires it.units.erallab.mrsim2d.core;
+  requires it.units.erallab.mrsim2d.builder;
+  requires it.units.erallab.robotevo2d.main;
   exports it.units.erallab.respap.hrefla to jcommander;
+  opens it.units.erallab.respap.hrefla to jcommander;
 }
